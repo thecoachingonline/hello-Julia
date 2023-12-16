3817ca93b644deafb206ff0a085843a4ceab3eed
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-16 01:20:31 +0000

    4096 Bits

--- hello.jl
+++ hello.jl
@@ -1,1 +1,21 @@
-println("Hello World")
+using RLWE
+
+# กำหนดค่าพารามิเตอร์ของอัลกอริทึม
+n = 4096
+q = 2^2048
+
+# แปลงข้อความเป็นตัวเลข
+message = "สวัสดีชาวโลก"
+message_int = string(message) |> collect |> map(x -> ord(x))
+
+# สร้างคีย์การเข้ารหัสและถอดรหัส
+key = RLWE.generate_key(n, q)
+
+# เข้ารหัสข้อความ
+ciphertext = RLWE.encrypt(key, message_int)
+
+# ถอดรหัสข้อความ
+plaintext = RLWE.decrypt(key, ciphertext)
+
+# แสดงข้อความที่ถอดรหัสได้
+println(plaintext)
